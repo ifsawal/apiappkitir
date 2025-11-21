0e432dced3ee726fb6a8fbca8873b8e1d5f39de4
ifsawal <ifsawal@gmail.com>
2025-11-21 18:45:47 +0700

    u

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -21,8 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-  
+        Carbon::setTimezone('Asia/Jakarta');
     }
-
-    
 }
